d77e381ea2ca558623979279dce3d6ee9bd3edc5
martineh@uji.es <martineh@tintorrum.local>
2017-11-10 17:58:35 +0100

    Update directories names

---
+++ fastlmm/association/global_vars.py
@@ -0,0 +1,19 @@
+global first_section
+global second_section
+
+global l_tot1
+global l_tot2
+global l_tot3
+global l_tot4
+global l_tot5
+
+
+first_section  = 0
+second_section = 0
+
+l_tot1 = 0
+l_tot2 = 0
+l_tot3 = 0
+l_tot4 = 0
+l_tot5 = 0
+
